93090886c938a564d3aba27d4094f877e41f31c8
Max <themaxhs@gmail.com>
2022-04-15 16:45:34 -0600

    tesla/clone/expo/finish/

---
+++ components/CarsList/cars.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    name: "Model 3",
+    tagLine: "Order Online for",
+    tagLineCTA: "Touchless Delivery",
+    image: require("../../assets/images/Model3.jpeg"),
+  },
+  {
+    name: "Model Y",
+    tagLine: "Order Online for",
+    tagLineCTA: "Touchless Delivery",
+    image: require("../../assets/images/ModelY.jpeg"),
+  },
+  {
+    name: "Model S",
+    tagLine: "Order Online for",
+    tagLineCTA: "Touchless Delivery",
+    image: require("../../assets/images/ModelS.jpeg"),
+  },
+  {
+    name: "Model X",
+    tagLine: "Order Online for",
+    tagLineCTA: "Touchless Delivery",
+    image: require("../../assets/images/ModelX.jpeg"),
+  },
+  {
+    name: "Solar Panels",
+    tagLine: "Lowest Cost Solar Panels in America",
+    image: require("../../assets/images/SolarPanels.jpeg"),
+  },
+  {
+    name: "Solar Roof",
+    tagLine: "Produce Clean Energy From Your Roof",
+    image: require("../../assets/images/SolarRoof.jpeg"),
+  },
+];
